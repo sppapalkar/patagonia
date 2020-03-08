86b31260fb3d4ac39f6d8a35128decd12f8d6d91
Sahil <spapalk@ncsu.edu>
2020-03-08 19:32:10 -0400

    Add name validations

--- patients/models.py
+++ patients/models.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.db import models
+from django.core.validators import RegexValidator
 from django.urls import reverse
+
+alpha = RegexValidator(r'^[A-Za-z][A-Zsa-z\s]*$', 'Only alphabets and spaces are allowed.')
 
 
 class Patient(models.Model):
-    first_name = models.CharField(blank=False, max_length=30, verbose_name='first name')
-    last_name = models.CharField(blank=False, max_length=30, verbose_name='last name')
+    first_name = models.CharField(blank=False, max_length=30, verbose_name='first name', validators=[alpha])
+    last_name = models.CharField(blank=False, max_length=30, verbose_name='last name', validators=[alpha])
     email = models.EmailField(blank=False, unique=True, verbose_name='email address')
     date_created = models.DateTimeField(auto_now_add=True)
     physician = models.ForeignKey(
--- users/models.py
+++ users/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
+
+alpha = RegexValidator(r'^[A-Za-z][A-Zsa-z\s]*$', 'Only alphabets and spaces are allowed.')
 
 
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first_name'), blank=False, max_length=30)
-    last_name = models.CharField(_('last_name'), blank=False, max_length=30)
+    first_name = models.CharField(blank=False, max_length=30, verbose_name='first name', validators=[alpha])
+    last_name = models.CharField(blank=False, max_length=30, verbose_name='last name', validators=[alpha])
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
