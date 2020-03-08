3aa5ee1209c32c25742098ccde9488755a835c83
Sahil <spapalk@ncsu.edu>
2020-03-08 03:54:55 -0400

    Add HTML wrappers

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib import messages
+# from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+
+
